f512a9077f34fc72e0e19c1bfa755d0146eb88ca
Art <absolutus11@gmail.com>
2024-07-31 17:53:21 +0400

    feat: add kafka producer and consumer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/abe511/kafka-service
+module kafka-go-service
 
 go 1.22.5
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
--- main.go
+++ main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
-	"github.com/abe511/kafka-service/handlers"
+	"kafka-go-service/handlers"
+	"kafka-go-service/kafkaservice"
 )
 
 
 func main() {
+
+	kafkaservice.InitKafka()
+
+	kafkaservice.RunConsumer()
+
+	// start a server with two endpoints
 	router := http.NewServeMux()
 	router.HandleFunc("GET /stats", handlers.GetStats)
-	router.HandleFunc("POST /message", handlers.SendMessage)
+	router.HandleFunc("POST /message", handlers.ReceiveMessage)
 
 	server := http.Server{
 		Addr: ":8080",
