b8a31fd0ba90b90b890445e3800b4727257afeb9
Art <absolutus11@gmail.com>
2024-07-31 17:52:33 +0400

    feat: add message receiver

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,13 +1,35 @@
 package handlers
 
 import (
+	"encoding/json"
+	"kafka-go-service/models"
+	"kafka-go-service/kafkaservice"
 	"net/http"
 )
 
 
-func SendMessage(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(r.Method + " message\n"))
+func ReceiveMessage(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("message received\n"))
+	var msg models.Message
+
+	// decode the msg from the request body json
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// forward the msg to kafka producer
+	err = kafkaservice.SendToKafka(&msg)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// respond with msg and status 201
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(msg)
 }
+
+
 
 func GetStats(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(r.Method + " stats\n"))
